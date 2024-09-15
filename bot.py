135bae81eab1eb2b66dd4382c83335002fb327cf
QQQiwi <ulitin.ivan3004@mail.ru>
2024-09-15 16:30:39 +0400

    feature | убрал говно за наилем

--- bot.py
+++
@@ -1,9 +0,0 @@
-#from langchain import OllamaLLM
-#from langchain_core.prompts import ChatPromptTemplate
-
-#model = OllamaLLM(model="llama3.1")
-
-#template = """
-
-#"""
-# тут будет привязка к олламе из веба!!!
