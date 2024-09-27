03d9efc7dd046741263264006ff6c4586b92e2aa
Niall Tugushev <nail.tugushev30@yandex.ru>
2024-09-27 15:18:58 +0400

    fix | Расширил логгирование.

--- run.py
+++ run.py
@@ -9,14 +9,31 @@ from app.users.handlers import router
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Настройка логирования
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 async def main():
+    logger.info("Запуск основного процесса...")
     dp.include_router(router)
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.error(f"Произошла ошибка во время работы бота: {e}")
+    finally:
+        logger.info("Поллинг остановлен.")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Завершение.")
+        logger.info("Завершение работы программы.")
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
+    finally:
+        logger.info("Запуск завершен.")
