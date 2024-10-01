a5ba2182b7191aac1304e89eab16e5c41f402161
ProstoLive <artem009saratov@gmail.com>
2024-10-01 09:08:00 +0400

    feat: Added single prompt request

--- main.py
+++ main.py
@@ -10,17 +10,35 @@ from config import MODELNAME, MODEL_URL
 app = FastAPI()
 
 
-class BotRequest(BaseModel):
+class BotRequestChat(BaseModel):
     user_id: int
     messages: list
 
+class BotRequestPrompt(BaseModel):
+    user_id: int
+    prompt: str
 
-@app.post("/request")
-async def request(request: BotRequest):
+class BotResponseChat(BaseModel):
+    message: dict
+    done: bool
+
+class BotResponsePrompt(BaseModel):
+    response: str
+    done: bool
+
+@app.post("/request_chat", response_model=BotResponseChat)
+async def request_chat(request: BotRequestChat):
     request_data = {"model": f"{MODELNAME}",
                     "messages": request.messages,
                     "stream": False}
-    answer = requests.post(f"{MODEL_URL}", data=json.dumps(request_data))
+    answer = requests.post(f"{MODEL_URL + '/api/chat'}", data=json.dumps(request_data))
     return answer.json()
 
+@app.post("/request_prompt", response_model=BotResponsePrompt)
+async def request_prompt(request: BotRequestPrompt):
+    request_data = {"model": f"{MODELNAME}",
+                    "prompt": request.prompt,
+                    "stream": False}
+    answer = requests.post(f"{MODEL_URL + '/api/generate'}", data=json.dumps(request_data))
+    return answer.json()
 
