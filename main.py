3a9af1a64345b8b7fa1a9055cf31f99c20367662
ProstoLive <artem009saratov@gmail.com>
2024-10-01 14:52:52 +0400

    feat: Added asynchronous requests to LLM

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import json
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import asyncio
+import aiohttp
+import grequests
 import requests
 
 from config import MODELNAME, MODEL_URL
-
 
 app = FastAPI()
 
@@ -14,31 +16,42 @@ class BotRequestChat(BaseModel):
     user_id: int
     messages: list
 
+
 class BotRequestPrompt(BaseModel):
     user_id: int
     prompt: str
+
 
 class BotResponseChat(BaseModel):
     message: dict
     done: bool
 
+
 class BotResponsePrompt(BaseModel):
     response: str
     done: bool
+
 
 @app.post("/request_chat", response_model=BotResponseChat)
 async def request_chat(request: BotRequestChat):
     request_data = {"model": f"{MODELNAME}",
                     "messages": request.messages,
                     "stream": False}
-    answer = requests.post(f"{MODEL_URL + '/api/chat'}", data=json.dumps(request_data))
-    return answer.json()
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{MODEL_URL + '/api/chat'}", data=json.dumps(request_data)) as answer:
+            answer = await answer.json()
+            session.close()
+            return answer
+
 
 @app.post("/request_prompt", response_model=BotResponsePrompt)
 async def request_prompt(request: BotRequestPrompt):
     request_data = {"model": f"{MODELNAME}",
                     "prompt": request.prompt,
                     "stream": False}
-    answer = requests.post(f"{MODEL_URL + '/api/generate'}", data=json.dumps(request_data))
-    return answer.json()
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{MODEL_URL + '/api/generate'}", data=json.dumps(request_data)) as answer:
+            answer = await answer.json()
+            session.close()
+            return answer
 
